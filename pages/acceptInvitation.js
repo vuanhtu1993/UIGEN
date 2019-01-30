a6a44c1c58e988907a197c7619478f11ab4b483e
AnhTu <tuva1@fsoft.com.vn>
2019-01-30 18:47:51 +0700

    test

---
+++ pages/acceptInvitation.js
@@ -0,0 +1,12 @@
+import BaseController from "./baseController";
+import React from "react";
+
+export default class AcceptInvitation extends BaseController {
+  render() {
+    return (
+      <div className='container'>
+        Login
+      </div>
+    )
+  }
+}
